cf99d10f97cfcc782c488573baf14ba67cdf2754
valimasca <e3skadron@gmail.com>
2019-09-23 16:15:29 -0700

    first commit here

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <ol>
+        <ul>
           <li>Vali</li>
           <li>Beth</li>
           <li>Mona</li>
-        </ol>
+        </ul>
+        <Person name="Vali" age="38" />
+        <Person name="Bethany" age="33">
+          <ul>My Hobbies:
+            <li>Reading</li>
+            <li>Riding</li>
+            <li>Watching TV</li>
+          </ul>
+        </Person>
+        <Person name="Mona" age="38" />
       </div>
     );
     // return React.createElement('div', null,
